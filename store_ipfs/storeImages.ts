3455733df9a5f9e7f8c4a42d27089a61c628726d
fu31 <torahikoshinoda0101@gmail.com>
2022-01-15 20:33:28 +0900

    added store_ipfs

---
+++ store_ipfs/storeImages.ts
@@ -0,0 +1,36 @@
+import fs from "fs";
+import { NFTStorage, File, toGatewayURL } from "nft.storage";
+
+require("dotenv").config();
+
+const endpoint = toGatewayURL("https://api.nft.storage");
+const token = process.env.NFT_STORAGE_API_KEY || "";
+const totalSupply =
+  process.env.TOTAL_SUPPLY !== undefined ? +process.env.TOTAL_SUPPLY : 0;
+
+async function main() {
+  if (totalSupply === 0) {
+    throw new Error("total supply is 0");
+  }
+
+  const storage = new NFTStorage({ endpoint, token });
+  let files: File[] = [];
+  for (let i = 0; i < totalSupply; i++) {
+    const num = i + 1;
+    const file = new File(
+      [
+        await fs.promises.readFile(
+          `../hashlips_art_engine/build/images/${num}.png`
+        ),
+      ],
+      `${num}.png`
+    );
+    files.push(file);
+  }
+  const cid = await storage.storeDirectory(files);
+  console.log({ cid });
+  const status = await storage.status(cid);
+  console.log(status);
+}
+
+main();
