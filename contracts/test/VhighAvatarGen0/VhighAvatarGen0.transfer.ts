c67108b4ff7a957909ab6e289cf8839ebcc446ef
fu31 <torahikoshinoda0101@gmail.com>
2022-01-15 15:22:17 +0900

    delete contracts

--- contracts/test/VhighAvatarGen0/VhighAvatarGen0.transfer.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "chai";
-
-export const shouldBehaveLikeAfterTransfer = () => {
-  it("should behave like after contract transfer", async function () {
-    expect(await this.nft.owner()).to.equal(
-      "0xF7Ebc23243bCB28ac70d6516AeC8785Ea74Fb9Cd"
-    );
-  });
-};
